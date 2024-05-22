ef1eefa24ddf15de22ba7d755805b32bf112d3df
raytechx <rayaneversiani@gmail.com>
2024-05-22 20:53:53 -0300

    Criando testes unitarios

---
+++ src/test/java/com/example/service/ProdutoServiceTest.java
@@ -0,0 +1,28 @@
+package com.example.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoServiceTest {
+
+    @Test
+    void obterTodos() {
+    }
+
+    @Test
+    void obterPorId() {
+    }
+
+    @Test
+    void adicionar() {
+    }
+
+    @Test
+    void deletar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+}
---
+++ src/test/java/com/example/view/controller/ProdutoControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.view.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProdutoControllerTest {
+
+    @Test
+    void obterTodos() {
+    }
+
+    @Test
+    void adicionar() {
+    }
+
+    @Test
+    void obterPorId() {
+    }
+
+    @Test
+    void deletar() {
+    }
+
+    @Test
+    void atualizar() {
+    }
+}
